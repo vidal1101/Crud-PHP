43b421ef6de286984076a3a54463792e9fab55f1
vidal-Software <rodrigovidal278@gmail.com>
2021-11-19 15:28:50 -0600

    creacion de editar completa. falta guardar la imagen

--- actualizar.php
+++ actualizar.php
@@ -3,10 +3,24 @@
 include("conexion.php");
 $con = Getconectarse();
 
+$id = $_POST["idlibro"];
+$titulo = $_POST['titulo'];
+$autor = $_POST['autor'];
+$categoria  = $_POST['categoria'];
 $fecha = $_POST["fecha"];
+$resumen = $_POST['resumen'];
+$imagen = "not image at the moment";
 
 
-echo $fecha;
+$sql = "UPDATE Libros SET Titulo= '$titulo' , Autor= '$autor' , Categoria='$categoria' , Fecha = '$fecha' , Resumen= '$resumen' , Imagen= '$imagen' WHERE idLibro = $id ";
+
+$query = mysqli_query($con , $sql);
+
+if($query){
+    Header("location: index.php ");
+}
+
+echo $id;
 
 
 
